be79c8aad6e09ea785f7c0ba82e8ef86a62cc62a
AlexLito666 <80423637+AlexLito666@users.noreply.github.com>
2021-04-25 17:00:54 +0400

    Add files via upload
    v 1.0

---
+++ lesson_3.py
@@ -0,0 +1,39 @@
+names = ["alex", "tom", "den"]
+print(names[::-1])
+
+name = "Alex"
+print(name[::-1])
+
+numbers = [1,2,3,4,5,3,23,4,1]
+print(list(set(numbers)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+names = ["alex", "tom", "den"]
+print(names[::-1])
+
+name = "ivan"
+print(name[::-1])
+
+numbers = [1,3,4,5,6,4,3,4,5]
+new_number = list(set(numbers))
+print(new_number)
+'''
